ece0a64d980da2115a2af1f7026b915011d76016
Ahmed Taher <tahertetoo@gmail.com>
2026-02-19 03:49:55 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,33 @@
-const CACHE_NAME = "gpa-app-v1";
+const CACHE_NAME="gpa-app-v2";
 
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./manifest.json"
+const urlsToCache=[
+"./",
+"./index.html",
+"./style.css",
+"./script.js",
+"./manifest.json"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install",e=>{
+self.skipWaiting();
+e.waitUntil(
+caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache))
+);
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener("activate",e=>{
+e.waitUntil(
+caches.keys().then(keys=>{
+return Promise.all(
+keys.filter(key=>key!==CACHE_NAME)
+.map(key=>caches.delete(key))
+);
+})
+);
+});
+
+self.addEventListener("fetch",e=>{
+e.respondWith(
+caches.match(e.request).then(res=>res||fetch(e.request))
+);
 });
