1997af823d2a175ff305b8abedc33a2241b86149
Ahmed Taher <tahertetoo@gmail.com>
2026-03-14 01:55:50 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "gpa-clean-v5";
+const APP_FILES = [
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json"
+];
+
+// Install
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
+  );
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Fetch (Network First بدون تخزين الصفحة نفسها)
+self.addEventListener("fetch", event => {
+
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+  );
+});
