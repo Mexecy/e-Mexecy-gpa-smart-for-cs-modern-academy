c95c9a21a09678d835e1beecd654fe2869cdb8fb
Ahmed Taher <tahertetoo@gmail.com>
2026-03-14 02:26:56 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,66 @@
-const CACHE_NAME = "gpa-clean-v5";
+const CACHE_NAME = "gpa-clean-v6";
+
 const APP_FILES = [
+  "./",
   "./index.html",
   "./style.css",
   "./script.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 // Install
 self.addEventListener("install", event => {
+
   self.skipWaiting();
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_FILES))
   );
+
 });
 
 // Activate
 self.addEventListener("activate", event => {
+
   event.waitUntil(
+
     caches.keys().then(keys =>
+
       Promise.all(
         keys
           .filter(key => key !== CACHE_NAME)
           .map(key => caches.delete(key))
       )
+
     ).then(() => self.clients.claim())
+
   );
+
 });
 
-// Fetch (Network First بدون تخزين الصفحة نفسها)
+// Fetch
 self.addEventListener("fetch", event => {
 
-  if (event.request.method !== "GET") return;
+  if(event.request.method !== "GET") return;
 
   event.respondWith(
+
     fetch(event.request)
+      .then(response => {
+
+        const clone = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(cache => cache.put(event.request, clone));
+
+        return response;
+
+      })
       .catch(() => caches.match(event.request))
+
   );
+
 });
